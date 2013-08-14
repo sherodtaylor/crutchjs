4a80993d19cd76ee4eb7b5a9cf5a8643d168e98d
Sherod Taylor <sherodtaylor@gmail.com>
2013-08-13 20:02:42 -0700

    initial commit

---
+++ tests/specs/events.js
@@ -0,0 +1,30 @@
+describe("Events", function (){
+  describe("On Event", function (){
+    it("be able attach events", function (){
+      object = {count: 0};
+      Crutch.extends(object, Crutch.Events);
+      object.on('count', counter);
+      function counter(){
+        this.count++;
+      };
+      expect(object._listeners).toEqual({ count: { callback: counter} });
+    });
+  });
+  describe("Trigger Event", function (){
+    it("triggers an event", function (){
+
+      object = {count: 0};
+      object.on('count', counter);
+      function counter(){
+        this.count++;
+      };
+      expect(object.count).toEqual(0);
+      object.trigger('count');
+      expect(object.count).toEqual(1);
+      object.trigger('count');
+      expect(object.count).toEqual(2);
+      object.trigger('count');
+      expect(object.count).toEqual(3);
+    });
+  });
+});
