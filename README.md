666307bce4f654b8f4aaf5d6b131475e7390423d
Sherod Taylor <sherodtaylor@gmail.com>
2013-08-13 20:59:04 -0700

    basic files added

---
+++ README.md
@@ -0,0 +1,4 @@
+Crutch
+
+A simple Javascript MVC framework. A very useful crutch for Javascript
+heavy applications.
--- tests/specs/events.js
+++ tests/specs/events.js
@@ -2,7 +2,7 @@ describe("Events", function (){
   describe("On Event", function (){
     it("be able attach events", function (){
       object = {count: 0};
-      Crutch.extends(object, Crutch.Events);
+      _.extend(object, Crutch.Events);
       object.on('count', counter);
       function counter(){
         this.count++;
@@ -12,8 +12,8 @@ describe("Events", function (){
   });
   describe("Trigger Event", function (){
     it("triggers an event", function (){
-
       object = {count: 0};
+      _.extend(object, Crutch.Events);
       object.on('count', counter);
       function counter(){
         this.count++;
@@ -25,6 +25,19 @@ describe("Events", function (){
       expect(object.count).toEqual(2);
       object.trigger('count');
       expect(object.count).toEqual(3);
+    });
+    it("triggers an event passing multiple variables", function (){
+      object = {count: 0};
+      _.extend(object, Crutch.Events);
+      object.on('count', counter);
+      function counter( start, iterations ){
+       this.count = this.count || start;
+        for ( var i = 0; i > iterations; ++i){
+          this.count++;
+        }
+      };
+      object.trigger('count', 1, 4);
+      expect(object.count).toEqual(5);
     });
   });
 });
