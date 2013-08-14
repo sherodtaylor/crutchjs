17a35ad10de88470d7d5ead33db9b01c0e6336b0
Sherod Taylor <sherodtaylor@gmail.com>
2013-08-14 03:23:21 -0700

    Events off function completed with passing specs build passes

--- tests/specs/events.js
+++ tests/specs/events.js
@@ -10,6 +10,7 @@ describe("Events", function (){
       expect(object._listeners).toEqual({ count: { callback: counter} });
     });
   });
+
   describe("Trigger Event", function (){
     it("triggers an event", function (){
       object = {count: 0};
@@ -38,6 +39,33 @@ describe("Events", function (){
       }
       object.trigger('count', 1, 4);
       expect(object.count).toEqual(5);
+    });
+  });
+
+  describe("Off", function (){
+    it("removes listener from object", function(){
+      object = {count: 0};
+      _.extend(object, Crutch.Events);
+      object.on('count', counter);
+      function counter(){
+        this.count++;
+      }
+      object.trigger('count');
+      expect(object.count).toEqual(1);
+
+      object.off('count');
+      object.trigger('count'); // shouldn't trigger the event
+      expect(object.count).toEqual(1);
+    });
+    it("should remove the property from the associative  array", function(){
+      object = {count: 0};
+      _.extend(object, Crutch.Events);
+      object.on('count', counter);
+      function counter(){
+        this.count++;
+      }
+      object.off('count');
+      expect(object._listeners.hasOwnProperty("count")).toBe(false);
     });
   });
 });
